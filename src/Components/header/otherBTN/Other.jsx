d5b609fe7266e7102c86f6a989f91f22658a4aac
Shahriar Amin Al Siphat <alsiphat30@gmail.com>
2024-03-20 11:02:11 +0600

    header profile done

--- src/Components/header/otherBTN/Other.jsx
+++ src/Components/header/otherBTN/Other.jsx
@@ -1,17 +1,28 @@
 import React, { useEffect, useRef, useState } from "react";
 import CartBTN from "./Cart/CartBTN";
 import CartContainer from "./Cart/CartContainer";
+import ProfileBTN from "./profile/ProfileBTN";
+import ProfileContainer from "./profile/ProfileContainer";
 
 function Other() {
   let CartRef = useRef();
+  let profileRef = useRef();
   const [cartModal, setCartModal] = useState(false);
+  const [profileModal, setProfileModal] = useState(false);
 
   useEffect(() => {
     document.addEventListener("click", (e) => {
-      if (CartRef.current.contains(e.target)) {
-        setCartModal(true);
+      if (CartRef.current.contains(e.target) || profileRef.current.contains(e.target)) {
+        if (CartRef.current.contains(e.target)) {
+          setCartModal(true);
+          setProfileModal(false);
+        }else{
+          setCartModal(false);
+          setProfileModal(true);
+        }
       } else {
         setCartModal(false);
+        setProfileModal(false);
       }
     });
   }, []);
@@ -23,9 +34,9 @@ function Other() {
           <CartBTN className="w-full hover:bg-gray hover:text-white  duration-500 cursor-pointer" />
           {cartModal && <CartContainer />}
         </div>
-        <div className="w-full">
-          <CartBTN className="w-full hover:bg-gray hover:text-white  duration-500 cursor-pointer" />
-          {/* <CartContainer /> */}
+        <div ref={profileRef} className="w-full">
+          <ProfileBTN className="w-full hover:bg-gray hover:text-white  duration-500 cursor-pointer" />
+          {profileModal && <ProfileContainer />}
         </div>
       </div>
     </>
--- src/Components/header/otherBTN/profile/ProfileContainer.jsx
+++ src/Components/header/otherBTN/profile/ProfileContainer.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import LinkPage from "../../../layout/LinkPage";
 
 function ProfileContainer() {
   return (
-    <div>ProfileContainer</div>
-  )
+    <>
+      <div className=" bg-Bg absolute top-[40px] right-0">
+        <div className="bg-black p-1 px-4 ">
+          <LinkPage
+            path={"/"}
+            txt={"My Account"}
+            className={"text-white font-bold "}
+          />
+        </div>
+        <div className=" p-1 px-4  hover:pl-5 duration-500">
+          <LinkPage
+            path={"/"}
+            txt={"Log Out"}
+            className={"font-semibold hover:text-red-600 duration-300"}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default ProfileContainer
+export default ProfileContainer;
